e5495b737e8de700a604bab2085bdefb742cf139
Dan Woodson <woodson.dan@gmail.com>
2023-09-06 17:29:45 -0600

    Adding delete UI

--- src/list-items/list-item/render/toHtmlString.ts
+++ src/list-items/list-item/render/toHtmlString.ts
@@ -1,11 +1,20 @@
-import type { Schema } from '../index.ts'
+import type { Schema } from '../validation.ts'
 
 // /**
 //  * Create HTML string from List Item
 //  * @param {Schema} li List Item to render
 //  * @returns {string} String of HTML representing the List Item
 //  */
-export const renderToHtmlString = (li: Schema, opts?: { wrap?: boolean }) =>
-  `${opts?.wrap ? '<ul>' : ''}<li title="${li.title}">${li.title}</li>${
-    opts?.wrap ? '</ul>' : ''
-  }`
+export const renderToHtmlString = (
+  li: Schema,
+  // XXX Should be {wrap: true, delete: {onclick: string, ...other attrs}, edit: { onclick: string, ...other attrs}}
+  opts?: { wrap?: boolean; deleteCallback: string }
+) => {
+  // console.log(li, opts)
+  const deleteUi = opts?.deleteCallback
+    ? `<button type="button" onclick="${opts.deleteCallback}">X</button>`
+    : ''
+  const mainElm = `<li title="${li.title}">${li.title}${deleteUi}</li>`
+
+  return (opts?.wrap ? '<ul>' : '') + mainElm + (opts?.wrap ? '</ul>' : '')
+}
