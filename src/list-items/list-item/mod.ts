27b2bdc58eabbffdedf8a64e60d3f88d87a64ade
Dan Woodson <woodson.dan@gmail.com>
2023-09-30 23:05:59 -0600

    Better typed functionality

--- src/list-items/list-item/mod.ts
+++ src/list-items/list-item/mod.ts
@@ -1,13 +1,37 @@
-import Storage from 'https://denopkg.com/ultraxlight/storage@main/src/local-storage.ts'
+import Storage from 'https://denopkg.com/ultraxlight/storage@main/implementations/local-storage.ts'
+import {ListItem as ListItemType} from './types.ts'
 
 const ListItem = {
-  ...Storage,
   create: (title: string) => {
     if (!title) {
       throw new TypeError('Missing title')
     }
 
-    return Storage.create({ title })
+    return Storage.create<ListItemType>({ title })
+  },
+  get: (id: string) => {
+    if (!id) {
+      throw new TypeError('Missing ID')
+    }
+
+    return Storage.get<ListItemType>(id)
+  },
+  getAll: () => {
+    return Storage.getAll<ListItemType>()
+  },
+  update: (id: string, updateObj: Partial<ListItemType>) => {
+    if (!id) {
+      throw new TypeError('Missing ID')
+    }
+
+    return Storage.update<ListItemType>(id, updateObj)
+  },
+  remove: (id: string) => {
+    if (!id) {
+      throw new TypeError('Missing ID')
+    }
+
+    return Storage.remove<ListItemType>(id)
   },
 }
 export default ListItem
---
+++ src/list-items/list-item/types.ts
@@ -0,0 +1,5 @@
+import {Item} from 'https://denopkg.com/ultraxlight/storage@main/src/types.ts'
+
+export interface ListItem extends Item {
+  title: string
+}
