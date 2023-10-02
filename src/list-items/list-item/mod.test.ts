b67e19072c75f9b996162b394479d8ab6327efa4
Dan Woodson <woodson.dan@gmail.com>
2023-10-01 21:44:21 -0600

    Correcting tests

--- src/list-items/list-item/mod.test.ts
+++ src/list-items/list-item/mod.test.ts
@@ -1,10 +1,8 @@
 import {
-  assert,
   assertEquals,
   assertThrows,
 } from 'https://deno.land/std@0.168.0/testing/asserts.ts'
 import ListItem from './mod.ts'
-import { Schema } from './validation.ts'
 
 Deno.test('Empty create call throws error', () => {
   // @ts-ignore This is meant to be called incorrectly
@@ -19,7 +17,7 @@ Deno.test('Create returns an object with a title', () => {
 
 Deno.test('Get can retrieve single', () => {
   const newItem = ListItem.create('Mow the lawn')
-  const retrievedItem = ListItem.get<Schema>(newItem.id)
+  const retrievedItem = ListItem.get(newItem.id)
   assertEquals(
     newItem.id,
     retrievedItem && !Array.isArray(retrievedItem) && retrievedItem.id
@@ -33,7 +31,7 @@ Deno.test('Get can retrieve single', () => {
 Deno.test('GetAll can retrieve multiple', () => {
   const newLi1 = ListItem.create('Mow the lawn')
   const newLi2 = ListItem.create('Mow the lawn 2')
-  const retrievedLis = ListItem.getAll<Schema>()
+  const retrievedLis = ListItem.getAll()
   const retrievedLi1 =
     Array.isArray(retrievedLis) &&
     retrievedLis.find((rLi) => rLi.id === newLi1.id)
@@ -53,7 +51,7 @@ Deno.test('GetAll can retrieve multiple', () => {
 
 Deno.test('remove removes', () => {
   const newItem = ListItem.create('Mow the lawn')
-  const retrievedItemBeforeRemove = ListItem.get<Schema>(newItem.id)
+  const retrievedItemBeforeRemove = ListItem.get(newItem.id)
 
   assertEquals(
     !Array.isArray(retrievedItemBeforeRemove) &&
@@ -63,52 +61,13 @@ Deno.test('remove removes', () => {
 
   ListItem.remove(newItem.id)
 
-  assertEquals(ListItem.get<Schema>(newItem.id), null)
+  assertEquals(ListItem.get(newItem.id), null)
 })
 
 Deno.test('remove returns removed', () => {
   const newItem = ListItem.create('Mow the lawn')
   const removedItem = ListItem.remove(newItem.id)
 
+  // @ts-ignore: fine if this doesn't work
   assertEquals(newItem, removedItem)
-})
-
-Deno.test('removeAll removes all', () => {
-  ListItem.create('Mow the lawn')
-  ListItem.create('Mow the lawn 2')
-
-  const items = ListItem.getAll()
-
-  assert(Array.isArray(items) && items.length > 1)
-
-  ListItem.removeAll()
-
-  assertEquals(ListItem.getAll<Schema>(), [])
-})
-
-Deno.test('removeAll returns all', () => {
-  const item1 = ListItem.create('Mow the lawn')
-  const item2 = ListItem.create('Mow the lawn 2')
-
-  const sortByTitle = (
-    a: Record<string, unknown>,
-    b: Record<string, unknown>
-  ) => {
-    if (
-      a.title &&
-      typeof a.title === 'string' &&
-      b.title &&
-      typeof b.title === 'string'
-    ) {
-      return a.title.length - b.title.length
-    }
-    return 0
-  }
-
-  const removed = ListItem.removeAll<{ title: string; id: string }>()
-
-  assertEquals(
-    [item1, item2].sort(sortByTitle),
-    (removed || []).sort(sortByTitle)
-  )
 })
